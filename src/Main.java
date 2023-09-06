057707b9d668e83efaf4575ed41f2fa0d658015f
HDPSjrwnsno <jerwin.hitachi.dps@gmail.com>
2023-09-06 16:25:26 +0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String [] args){
+        println("Hello My Name is Jerwin Sano");
+
+    }
+    public static void println(String n){
+        System.out.println(n);
+        return;
+    }
+
+}
