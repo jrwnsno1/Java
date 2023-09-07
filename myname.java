55be9b2148a1685af9994f5c04ba14b1f8b67998
HDPSjrwnsno <jerwin.hitachi.dps@gmail.com>
2023-09-07 11:07:40 +0800

    2nd

---
+++ myname.java
@@ -0,0 +1,10 @@
+public class Myname{
+    public static void main(String [] args){
+        println("Hello My Name is Jerwin");
+    }
+    public static void println(String n){
+        System.out.println(n);
+        return;
+    }
+
+}
